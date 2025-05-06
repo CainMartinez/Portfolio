16629e9b0adc418d6e9eddc01eab852825b64718
Cain <cainmartinez2@hotmail.com>
2025-05-06 23:06:01 +0000

    Remove unused import of useEffect in Experience component

--- next/src/app/components/Experience.tsx
+++ next/src/app/components/Experience.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { FaBriefcase, FaCalendarAlt, FaMapMarkerAlt, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 interface ExperienceItem {
